48ff610543de2cf387197e420e9e9fd083af2158
ILEF <maherkhorchfi@hotmail.com>
2021-04-06 15:06:24 +0100

    check_point_jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import imageInSrc from './imageInSrc.jpg';
 import './App.css';
+import './style.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+  <div style={{border: 'solid 1px black', maxWidth: '100vw'}}>
+    <h1 className="title red">Dream trip to Thailand</h1>
+    <img src={imageInSrc}  alt="image" className="center"/>
+    <p className="text">By the richness of its culture, its turquoise waters, its luxuriant flora and fauna, Thailand is one of the most popular tourist destinations. Tiga will go to the "land of smiles" in the lands of a Thailand full of contrasts and contradictions ...</p>
+    <img src="/imageInPublic.jpg" className="center"/>
+  </div>
+  <h2 className="titleVideo">Enjoy watching..</h2>
+  <div className="center"><video  controls>
+    <source src="myVideo.mp4" type="video/mp4" />
+  </video>
+  </div>
+
+</>
+    
   );
 }
 
